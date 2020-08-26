39e0457a56d8cea58fd08183a3d4db877a363db6
MatStat <powdersteamboat@gmail.com>
2020-08-25 22:46:10 -0600

    Updates

---
+++ precache-manifest.4c3f16189fe3397cedab239b7d091c45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7b328d23798e706a0444a1600eaf40",
+    "url": "/MStadtler21.github.io/Employee-Directory-React/index.html"
+  },
+  {
+    "revision": "00d0172ec6d591bfe54a",
+    "url": "/MStadtler21.github.io/Employee-Directory-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03adfed2a5ff7d41ecb6",
+    "url": "/MStadtler21.github.io/Employee-Directory-React/static/js/2.6e8f5727.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MStadtler21.github.io/Employee-Directory-React/static/js/2.6e8f5727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d0172ec6d591bfe54a",
+    "url": "/MStadtler21.github.io/Employee-Directory-React/static/js/main.049adc1b.chunk.js"
+  },
+  {
+    "revision": "6e839badfd6b74d40753",
+    "url": "/MStadtler21.github.io/Employee-Directory-React/static/js/runtime-main.64679d76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MStadtler21.github.io/Employee-Directory-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
